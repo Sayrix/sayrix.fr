ac0c440da639e87ccea16d10f38bc1744c9b8570
Sayrix <xaviercraft99@gmail.com>
2021-11-11 20:51:46 +0100

    Thanks IchiiDev ^^

---
+++ assets/config.js
@@ -0,0 +1,33 @@
+const config = {
+    
+    USERNAME: "Siri#1111",
+    /**
+     * This can either be a file link (relative or external)
+     * or a Gravatar email or hash.
+     */
+    PICTURE: "https://cdn.sayrix.fr/J1t/siri.gif",
+
+    DESCRIPTION: "Hi 👋, I'm Sayrix ! I am French and i live in Bordeaux 🥖 I'm Brand Designer & NodeJS Developer",
+
+    BADGES: [
+        { id: "HypeSquad Balance", image: "https://cdn.sayrix.fr/J1t/hypesquad_balance.svg" },
+        { id: "Early Verified Bot Developer", image: "https://cdn.sayrix.fr/J1t/dev.svg" },
+        { id: "Nitro Sub", image: "https://cdn.sayrix.fr/J1t/nitro.svg" },
+        { id: "Nitro Boosting 3 Months Badge", image: "https://cdn.sayrix.fr/J1t/nitro_boost.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "ChilledBot", image: "https://cdn.sayrix.fr/J1t/ChilledBot2%20Rounded.png", link: "https://chilledbot.xyz", name: "ChilledBot, a discord bot playing lofi hip hop music 24/7" },
+        { id: "U'World", image: "https://cdn.sayrix.fr/J1t/uworld.png", link: "https://uworldmc.fr", name: "U'World, a Minecraft Survival and OneBlock server in 1.17.1" },
+        { id: "Portfolio", image: "https://cdn.sayrix.fr/J1t/S.png", link: "https://sayrix.fr/portfolio", name: "My portfolio" },
+        { id: "Other Projects", image: "https://cdn.sayrix.fr/J1t/invisible.png", link: "", name: "<a href=\"https://dsc.gg/uty\" target=\"_blank\">Uty</a>, a discord bot with mini-games<br\><a href=\"https://top.gg/bot/853042562250965022\" target=\"_blank\">BetterSlowmode</a>, a discord bot allowing to have a personalized slowmode" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Sayrix" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/SayrixFX" },
+        { class_name: "fab fa-discord", link: "https://discord.gg/VasYV6MEJy" },
+        { class_name: "fab fa-twitch", link: "https://twitch.tv/s4yrix" },
+    ],
+
+}
